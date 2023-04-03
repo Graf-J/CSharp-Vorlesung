b7c8ff2e252b20fd5c164bc967c971cc7b369b23
Graf-J <johannes.graf.1908@gmail.com>
2023-04-03 16:28:17 +0200

    Add Aufgabe_2

---
+++ Aufgabe_2/Auto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_2
+{
+    abstract internal class Auto
+    {
+        protected int numDoors;
+        public string? Colour { get; set; }
+        public string? Type { get; set; }
+
+        public int GetNumberOfDoors()
+        {
+            return numDoors;
+        }
+    }
+}
---
+++ Aufgabe_2/Van.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_2
+{
+    internal class Van : Kombi
+    {
+        private bool hasLoadArea;
+        public bool HasLoadArea
+        {
+            set 
+            {
+                numDoors = value ? 2 : 5;
+                hasLoadArea = value;
+            }
+        }
+    }
+}
