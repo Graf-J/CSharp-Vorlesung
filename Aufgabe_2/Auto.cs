04817b5e17256445361ec823a0e81290331d685b
Graf-J <johannes.graf.1908@gmail.com>
2023-04-17 13:32:14 +0200

    Add Comments to Aufgabe2

--- Aufgabe_2/Auto.cs
+++ Aufgabe_2/Auto.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace Aufgabe_2
 {
+    // Jede Klasse erbt von Object Basisklasse (beinhaltet grundlegende Funktionen)
     abstract internal class Auto
     {
         protected int numDoors;
         public string? Colour { get; set; }
         public string? Type { get; set; }
+
+        // Hier würde sich evtl. prozected Konstruktor anbieten
+        // -> über Konstruktorerbung aufrufbar
 
         public int GetNumberOfDoors()
         {
